5b1e39504f8dc3c867c0be0527b4e47b3c4c4681
Christian Gullord <gullord.christian@gmail.com>
2022-10-15 23:04:06 -0500

    New Project

--- pages/crypto-app.jsx
+++ pages/crypto-app.jsx
@@ -35,7 +35,7 @@ const crypto = () => {
 					<Link
 						target="_blank"
 						rel="noopener noreferrer"
-						href="https://github.com/Gullord-Christian/Grand_Exchange">
+						href="https://github.com/Gullord-Christian/Crypto-Press">
 						<button className="px-8 py-2 mt-4 mr-8">Code</button>
 					</Link>
 					<Link
