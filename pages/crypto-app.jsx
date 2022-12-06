45190c03ec743376aa4dab119be8eff5d578b43a
Christian Gullord <gullord.christian@gmail.com>
2022-12-05 23:16:29 -0600

    Adding credentials

--- pages/crypto-app.jsx
+++ pages/crypto-app.jsx
@@ -51,9 +51,7 @@ const crypto = () => {
 				</div>
 				<div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
 					<div className="p-2">
-						<p className="text-center font-bold pb-2 mt-6">
-							Technologies
-						</p>
+						<p className="text-center font-bold pb-2">Technologies</p>
 						<div className="grid grid-cols-3 md:grid-cols-1">
 							<p className="text-gray-600 py-2 flex items-center">
 								<RiRadioButtonFill className="pr-1" />
