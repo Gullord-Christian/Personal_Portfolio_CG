999425fc001c9e8c6136e776b0535f046032cd77
Christian Gullord <gullord.christian@gmail.com>
2023-07-11 17:27:08 -0500

    added new profile picture and updated info

--- components/About.jsx
+++ components/About.jsx
@@ -3,48 +3,48 @@ import React from "react";
 import Link from "next/link";
 
 const About = () => {
-	return (
-		<div>
-			<div className="w-[75%] h-[65%] lg:w-[65%] m-auto shadow-xl shadow-gray-400 rounded-xl items-center hover:scale-105 align-center ease-in duration-300">
-				<img
-					className="rounded-xl w-full md:mt-0 mt-6"
-					src={"/static/images/unsplash-tech-2.jpg"}
-					alt="laptop"
-				/>
-			</div>
-			<div
-				id="about"
-				className="w-full h-[90vh] md:h-screen p-2 flex items-center py-16 layer1">
-				<div className="m-auto max-w-[80%] ">
-					<div className="col-span-2">
-						<h2 className="uppercase text-xl tracking-widest text-[#008B8B] py-4">
-							About Me
-						</h2>
-						<p className="py-2 text-gray-600">
-							Prior to software development, I spent the last 10 years
-							working in the hospitality industry. From working in the
-							wine & restaurant industry in my hometown of Napa Valley
-							to most recently enjoying the beach life while managing
-							and operating luxury resorts in San Diego.
-						</p>{" "}
-						<p className="py-2 text-gray-600">
-							I&apos;ve always had an interest in computers, machines,
-							video games, and just a natural curiosity about all
-							things&apos; related to technology. While taking a few
-							introductory courses, I realized I have a passion for
-							programming and decided to pursue a career in software
-							development.
-						</p>
-						<p className="py-2 text-gray-600 underline cursor-pointer md:block hidden">
-							<Link href="/#projects">
-								Check out some of my latest projects
-							</Link>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="w-[75%] h-[65%] lg:w-[65%] m-auto shadow-xl shadow-gray-400 rounded-xl items-center hover:scale-105 align-center ease-in duration-300">
+        <img
+          className="rounded-xl w-full md:mt-0 mt-6"
+          src={"/static/images/unsplash-tech-2.jpg"}
+          alt="laptop"
+        />
+      </div>
+      <div
+        id="about"
+        className="w-full h-[90vh] md:h-screen p-2 flex items-center py-16 layer1"
+      >
+        <div className="m-auto max-w-[80%] ">
+          <div className="col-span-2">
+            <h2 className="uppercase text-xl tracking-widest text-[#008B8B] py-4">
+              About Me
+            </h2>
+            <p className="py-2 text-gray-600">
+              Over the past 10 years, I've had the pleasure of working in the
+              vibrant hospitality industry. From my roots in the wine and
+              restaurant scene of Napa Valley to managing luxury resorts in
+              sunny San Diego, it's been an incredible journey.
+            </p>{" "}
+            <p className="py-2 text-gray-600">
+              I&apos;ve always had an interest in computers, machines, video
+              games, and just a natural curiosity about all things&apos; related
+              to technology. After dipping my toes in a few introductory
+              courses, I knew I had found my true passion: software development.
+              It's the perfect blend of my interests and skills, and I can't
+              wait to dive deeper into this field and build a career out of it.
+            </p>
+            <p className="py-2 text-gray-600 underline cursor-pointer md:block hidden">
+              <Link href="/#projects">
+                Check out some of my latest projects
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,259 +10,253 @@ import { useRouter } from "next/router";
 import { toast } from "react-hot-toast";
 
 const Contact = () => {
-	const router = useRouter();
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-	const onSubmitForm = async (values) => {
-		let config = {
-			method: "post",
-			url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
-			headers: {
-				"Content-Type": "application/json",
-			},
-			data: values,
-		};
+  const onSubmitForm = async (values) => {
+    let config = {
+      method: "post",
+      url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: values,
+    };
 
-		try {
-			const response = await axios(config);
-			if (response.status == 200) {
-				reset();
-				toast.success(
-					"Your message has been sent! \n Thank you for contacting me, I will reach out as soon as I can!",
-					{
-						position: "bottom-center",
-					}
-				);
-			}
-		} catch (err) {
-			console.error(err);
-			toast.error(
-				"Uh oh! Something went wrong. Please try to send your message again"
-			);
-		}
-	};
+    try {
+      const response = await axios(config);
+      if (response.status == 200) {
+        reset();
+        toast.success(
+          "Your message has been sent! \n Thank you for contacting me, I will reach out as soon as I can!",
+          {
+            position: "bottom-center",
+          }
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error(
+        "Uh oh! Something went wrong. Please try to send your message again"
+      );
+    }
+  };
 
-	return (
-		<div id="contact" className="w-full lg:h-screen">
-			<div className="max-w-[1240px] m-auto px-2 py-16 w-full">
-				<p className="text-xl tracking-widest uppercase ml-2 text-[#008B8B]">
-					Contact
-				</p>
-				<h2 className="py-4">Get In Touch</h2>
-				<div className="grid md:grid-cols-5 gap-6">
-					<div className="col-span-3 md:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4 ">
-						<div className="lg:p-4 h-full">
-							<div className="flex">
-								<img
-									className="rounded-xl hover:scale-105 ease-in duration-300 md:w-[75%] md:h-[75%] md:mx-auto lg:h-full lg:w-full h-[85%] w-[85%] mx-auto "
-									src={"/static/images/profile-pic-4.jpg"}
-									alt="contact"
-								/>
-							</div>
-							<div>
-								<h2 className="py-4 sm:text-3xl text-xl">
-									Christian Gullord
-								</h2>
-								<p>Software Developer</p>
-								<p className="py-4">
-									I am currently seeking a full-stack, front-end or
-									back-end developer role.
-								</p>
-							</div>
-							<div>
-								<p className="uppercase pt-8 pb-4 text-center text-[#2c5074] font-semibold text-base">
-									Connect with me
-								</p>
-								<div>
-									<div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-										<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
-											<Link
-												target="_blank"
-												href="https://www.linkedin.com/in/christiangullord/">
-												<FaLinkedinIn />
-											</Link>
-										</div>
-										<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
-											<Link
-												target="_blank"
-												href="https://github.com/Gullord-Christian">
-												<FaGithub />
-											</Link>
-										</div>
-										<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
-											<Link
-												target="_blank"
-												href="mailto:gullord.christian@gmail.com">
-												<AiOutlineMail />
-											</Link>
-										</div>
-										<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
-											<BsFillPersonLinesFill />
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
-						<div className="p-4">
-							<form onSubmit={handleSubmit(onSubmitForm)}>
-								<div className="grid md:grid-cols-2 gap-4 w-full py-2">
-									<div className="flex flex-col">
-										<label
-											className="uppercase text-sm py-2"
-											htmlFor="name"
-											id="name">
-											Name
-										</label>
-										<input
-											className={`border-2 rounded-lg p-3 flex border-gray ${
-												errors.name
-													? "ring-2 ring-red-500"
-													: null
-											}`}
-											type="text"
-											name="name"
-											{...register("name", {
-												required: {
-													value: true,
-													message:
-														"Please enter your name",
-												},
-											})}
-										/>
-										<span className="text-red-400 text-[15px] py-2">
-											{errors?.name?.message}
-										</span>
-									</div>
-									<div className="flex flex-col">
-										<label
-											className="uppercase text-sm py-2"
-											htmlFor="phone"
-											id="phoneNumber">
-											Phone Number{" "}
-											<span className="text-gray-400">
-												(Optional)
-											</span>
-										</label>
-										<input
-											className="border-2 rounded-lg p-3 flex border-gray"
-											type="tel"
-											name="phoneNumber"
-											{...register("phoneNumber", {
-												required: false,
-											})}
-										/>
-									</div>
-								</div>
-								<div className="flex flex-col py-2">
-									<label
-										className="uppercase text-sm py-2"
-										htmlFor="email"
-										id="email">
-										Email
-									</label>
-									<input
-										className={`border-2 rounded-lg p-3 flex border-gray ${
-											errors.name
-												? "ring-2 ring-red-500"
-												: null
-										}`}
-										type="text"
-										name="email"
-										{...register("email", {
-											required: {
-												value: true,
-												message: "Please enter your email",
-											},
-											pattern: {
-												value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/,
-												message:
-													"Please enter valid email address",
-											},
-										})}
-									/>
-									<span className="text-red-400 text-[15px] py-2">
-										{errors?.email?.message}
-									</span>
-								</div>
-								<div className="flex flex-col py-2">
-									<label
-										className="uppercase text-sm py-2"
-										htmlFor="subject"
-										id="subject">
-										Subject{" "}
-										<span className="text-gray-400">
-											(Optional)
-										</span>
-									</label>
-									<input
-										className="border-2 rounded-lg p-3 flex border-gray"
-										type="text"
-										name="subject"
-										{...register("subject", {
-											required: false,
-										})}
-									/>
-								</div>
-								<div className="flex flex-col py-2">
-									<label
-										className="uppercase text-sm py-2"
-										htmlFor="message"
-										id="message">
-										Message
-									</label>
-									<textarea
-										className={`border-2 rounded-lg p-3 flex border-gray ${
-											errors.name
-												? "ring-2 ring-red-500"
-												: null
-										}`}
-										rows="6"
-										type="text"
-										name="message"
-										{...register("message", {
-											required: {
-												value: true,
-												message: "Please enter your message",
-											},
-											maxLength: {
-												value: 1000,
-												message: "Your message is too long",
-											},
-											minLength: {
-												value: 5,
-												message:
-													"Your message needs to be at least 5 characters",
-											},
-										})}
-									/>
-									<span className="text-red-400 text-[15px] py-2">
-										{errors?.message?.message}
-									</span>
-								</div>
-								<button className="w-full p-4 text-gray-100 mt-10 hover:ring-2">
-									Send Message
-								</button>
-							</form>
-						</div>
-					</div>
-				</div>
-				<div className="flex justify-center pt-12">
-					<Link href="/">
-						<div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
-							<HiOutlineChevronDoubleUp className="text-[#2c5074] size={30} hover:text-[#2c5074]" />
-						</div>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div id="contact" className="w-full lg:h-screen">
+      <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
+        <p className="text-xl tracking-widest uppercase ml-2 text-[#008B8B]">
+          Contact
+        </p>
+        <h2 className="py-4">Get In Touch</h2>
+        <div className="grid md:grid-cols-5 gap-6">
+          <div className="col-span-3 md:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4 ">
+            <div className="lg:p-4 h-full">
+              <div className="flex">
+                <img
+                  className="rounded-xl hover:scale-105 ease-in duration-300 md:w-[75%] md:h-[75%] md:mx-auto lg:h-full lg:w-full h-[85%] w-[85%] mx-auto "
+                  src={"/static/images/profile_pic_personal_portfolio.png"}
+                  alt="contact"
+                />
+              </div>
+              <div>
+                <h2 className="py-4 sm:text-3xl text-xl">Christian Gullord</h2>
+                <p>Software Developer</p>
+                <p className="py-4">
+                  In my current role, I am working as a full-stack developer.
+                  Our stack includes PHP, Laravel, Vue.js and SQL.
+                </p>
+              </div>
+              <div>
+                <p className="uppercase pt-8 pb-4 text-center text-[#2c5074] font-semibold text-base">
+                  Connect with me
+                </p>
+                <div>
+                  <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
+                      <Link
+                        target="_blank"
+                        href="https://www.linkedin.com/in/christiangullord/"
+                      >
+                        <FaLinkedinIn />
+                      </Link>
+                    </div>
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
+                      <Link
+                        target="_blank"
+                        href="https://github.com/Gullord-Christian"
+                      >
+                        <FaGithub />
+                      </Link>
+                    </div>
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
+                      <Link
+                        target="_blank"
+                        href="mailto:gullord.christian@gmail.com"
+                      >
+                        <AiOutlineMail />
+                      </Link>
+                    </div>
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300 hover:text-[#2c5074]">
+                      <BsFillPersonLinesFill />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form onSubmit={handleSubmit(onSubmitForm)}>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label
+                      className="uppercase text-sm py-2"
+                      htmlFor="name"
+                      id="name"
+                    >
+                      Name
+                    </label>
+                    <input
+                      className={`border-2 rounded-lg p-3 flex border-gray ${
+                        errors.name ? "ring-2 ring-red-500" : null
+                      }`}
+                      type="text"
+                      name="name"
+                      {...register("name", {
+                        required: {
+                          value: true,
+                          message: "Please enter your name",
+                        },
+                      })}
+                    />
+                    <span className="text-red-400 text-[15px] py-2">
+                      {errors?.name?.message}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      className="uppercase text-sm py-2"
+                      htmlFor="phone"
+                      id="phoneNumber"
+                    >
+                      Phone Number{" "}
+                      <span className="text-gray-400">(Optional)</span>
+                    </label>
+                    <input
+                      className="border-2 rounded-lg p-3 flex border-gray"
+                      type="tel"
+                      name="phoneNumber"
+                      {...register("phoneNumber", {
+                        required: false,
+                      })}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label
+                    className="uppercase text-sm py-2"
+                    htmlFor="email"
+                    id="email"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className={`border-2 rounded-lg p-3 flex border-gray ${
+                      errors.name ? "ring-2 ring-red-500" : null
+                    }`}
+                    type="text"
+                    name="email"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Please enter your email",
+                      },
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/,
+                        message: "Please enter valid email address",
+                      },
+                    })}
+                  />
+                  <span className="text-red-400 text-[15px] py-2">
+                    {errors?.email?.message}
+                  </span>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label
+                    className="uppercase text-sm py-2"
+                    htmlFor="subject"
+                    id="subject"
+                  >
+                    Subject <span className="text-gray-400">(Optional)</span>
+                  </label>
+                  <input
+                    className="border-2 rounded-lg p-3 flex border-gray"
+                    type="text"
+                    name="subject"
+                    {...register("subject", {
+                      required: false,
+                    })}
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label
+                    className="uppercase text-sm py-2"
+                    htmlFor="message"
+                    id="message"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    className={`border-2 rounded-lg p-3 flex border-gray ${
+                      errors.name ? "ring-2 ring-red-500" : null
+                    }`}
+                    rows="6"
+                    type="text"
+                    name="message"
+                    {...register("message", {
+                      required: {
+                        value: true,
+                        message: "Please enter your message",
+                      },
+                      maxLength: {
+                        value: 1000,
+                        message: "Your message is too long",
+                      },
+                      minLength: {
+                        value: 5,
+                        message:
+                          "Your message needs to be at least 5 characters",
+                      },
+                    })}
+                  />
+                  <span className="text-red-400 text-[15px] py-2">
+                    {errors?.message?.message}
+                  </span>
+                </div>
+                <button className="w-full p-4 text-gray-100 mt-10 hover:ring-2">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center pt-12">
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#2c5074] size={30} hover:text-[#2c5074]" />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
